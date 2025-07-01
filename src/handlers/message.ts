b408fb06b221a8fc342615f38a8752fd5e51c09e
Daniel Lucas Costa Rodrigues <daniel.l.c.rodrigues@gmail.com>
2025-06-30 23:42:05 -0300

    @vaizerds

--- src/handlers/message.ts
+++ src/handlers/message.ts
@@ -1,172 +1,53 @@
-import { Message } from "whatsapp-web.js";
-import { startsWithIgnoreCase } from "../utils";
-
-// Config & Constants
+import { Client, Message } from "whatsapp-web.js";
 import config from "../config";
-
-// CLI
 import * as cli from "../cli/ui";
-
-// ChatGPT & DALLE
-import { handleMessageGPT, handleDeleteConversation } from "../handlers/gpt";
-import { handleMessageDALLE } from "../handlers/dalle";
-import { handleMessageAIConfig, getConfig, executeCommand } from "../handlers/ai-config";
-import { handleMessageLangChain } from "../handlers/langchain";
-
-// Speech API & Whisper
-import { TranscriptionMode } from "../types/transcription-mode";
-import { transcribeRequest } from "../providers/speech";
-import { transcribeAudioLocal } from "../providers/whisper-local";
-import { transcribeWhisperApi } from "../providers/whisper-api";
-import { transcribeOpenAI } from "../providers/openai";
-
-// For deciding to ignore old messages
+import { startsWithIgnoreCase } from "../utils";
+import { handleMessageGPT, handleDeleteConversation } from "./gpt";
+import { handleMessageDALLE } from "./dalle";
+import { handleMessageAIConfig, getConfig, executeCommand } from "./ai-config";
 import { botReadyTimestamp } from "../index";
 
-// Handles message
-async function handleIncomingMessage(message: Message) {
-	let messageString = message.body;
+export async function handleIncomingMessage(client: Client, message: Message) {
+  const messageString = message.body;
 
-	// Prevent handling old messages
-	if (message.timestamp != null) {
-		const messageTimestamp = new Date(message.timestamp * 1000);
+  if (message.timestamp != null) {
+    const messageTimestamp = new Date(message.timestamp * 1000);
+    if (!botReadyTimestamp || messageTimestamp < botReadyTimestamp) {
+      cli.print("Ignoring old message: " + messageString);
+      return;
+    }
+  }
 
-		// If startTimestamp is null, the bot is not ready yet
-		if (botReadyTimestamp == null) {
-			cli.print("Ignoring message because bot is not ready yet: " + messageString);
-			return;
-		}
+  if ((await message.getChat()).isGroup && !config.groupchatsEnabled) return;
 
-		// Ignore messages that are sent before the bot is started
-		if (messageTimestamp < botReadyTimestamp) {
-			cli.print("Ignoring old message: " + messageString);
-			return;
-		}
-	}
+  const selfNotedMessage = message.fromMe && !message.hasQuotedMsg && message.from === message.to;
 
-	// Ignore groupchats if disabled
-	if ((await message.getChat()).isGroup && !config.groupchatsEnabled) return;
+  if (config.whitelistedEnabled) {
+    const whitelist = getConfig("general", "whitelist");
+    if (!selfNotedMessage && whitelist.length > 0 && !whitelist.includes(message.from)) {
+      cli.print(`Ignoring message from ${message.from} (not whitelisted).`);
+      return;
+    }
+  }
 
-	const selfNotedMessage = message.fromMe && message.hasQuotedMsg === false && message.from === message.to;
+  if (startsWithIgnoreCase(messageString, config.resetPrefix)) {
+    await handleDeleteConversation(client, message);
+    return;
+  }
 
-	if (config.whitelistedEnabled) {
-		const whitelistedPhoneNumbers = getConfig("general", "whitelist");
+  if (startsWithIgnoreCase(messageString, config.aiConfigPrefix)) {
+    const prompt = messageString.substring(config.aiConfigPrefix.length + 1);
+    await handleMessageAIConfig(client, message, prompt);
+    return;
+  }
 
-		if (!selfNotedMessage && whitelistedPhoneNumbers.length > 0 && !whitelistedPhoneNumbers.includes(message.from)) {
-			cli.print(`Ignoring message from ${message.from} because it is not whitelisted.`);
-			return;
-		}
-	}
-	// Transcribe audio
-	if (message.hasMedia) {
-		const media = await message.downloadMedia();
+  if (startsWithIgnoreCase(messageString, config.gptPrefix)) {
+    const prompt = messageString.substring(config.gptPrefix.length + 1);
+    await handleMessageGPT(client, message, prompt);
+    return;
+  }
 
-		// Ignore non-audio media
-		if (!media || !media.mimetype.startsWith("audio/")) return;
-
-		// Check if transcription is enabled (Default: false)
-		if (!getConfig("transcription", "enabled")) {
-			cli.print("[Transcription] Received voice messsage but voice transcription is disabled.");
-			return;
-		}
-
-		// Convert media to base64 string
-		const mediaBuffer = Buffer.from(media.data, "base64");
-
-		// Transcribe locally or with Speech API
-		const transcriptionMode = getConfig("transcription", "mode");
-		cli.print(`[Transcription] Transcribing audio with "${transcriptionMode}" mode...`);
-
-		let res;
-		switch (transcriptionMode) {
-			case TranscriptionMode.Local:
-				res = await transcribeAudioLocal(mediaBuffer);
-				break;
-			case TranscriptionMode.OpenAI:
-				res = await transcribeOpenAI(mediaBuffer);
-				break;
-			case TranscriptionMode.WhisperAPI:
-				res = await transcribeWhisperApi(new Blob([mediaBuffer]));
-				break;
-			case TranscriptionMode.SpeechAPI:
-				res = await transcribeRequest(new Blob([mediaBuffer]));
-				break;
-			default:
-				cli.print(`[Transcription] Unsupported transcription mode: ${transcriptionMode}`);
-		}
-		const { text: transcribedText, language: transcribedLanguage } = res;
-
-		// Check transcription is null (error)
-		if (transcribedText == null) {
-			message.reply("I couldn't understand what you said.");
-			return;
-		}
-
-		// Check transcription is empty (silent voice message)
-		if (transcribedText.length == 0) {
-			message.reply("I couldn't understand what you said.");
-			return;
-		}
-
-		// Log transcription
-		cli.print(`[Transcription] Transcription response: ${transcribedText} (language: ${transcribedLanguage})`);
-
-		// Reply with transcription
-		if (config.ttsTranscriptionResponse) {
-			const reply = `You said: ${transcribedText}${transcribedLanguage ? " (language: " + transcribedLanguage + ")" : ""}`;
-			message.reply(reply);
-		}
-
-		// Handle message GPT
-		await handleMessageGPT(message, transcribedText);
-		return;
-	}
-
-	// Clear conversation context (!clear)
-	if (startsWithIgnoreCase(messageString, config.resetPrefix)) {
-		await handleDeleteConversation(message);
-		return;
-	}
-
-	// AiConfig (!config <args>)
-	if (startsWithIgnoreCase(messageString, config.aiConfigPrefix)) {
-		const prompt = messageString.substring(config.aiConfigPrefix.length + 1);
-		await handleMessageAIConfig(message, prompt);
-		return;
-	}
-
-	// GPT (!gpt <prompt>)
-	if (startsWithIgnoreCase(messageString, config.gptPrefix)) {
-		const prompt = messageString.substring(config.gptPrefix.length + 1);
-		await handleMessageGPT(message, prompt);
-		return;
-	}
-
-	// GPT (!lang <prompt>) — Desativado temporariamente por falta de SerpAPI
-	if (startsWithIgnoreCase(messageString, config.langChainPrefix)) {
-	message.reply("⚠️ O recurso LangChain está temporariamente desativado (Sem SerpAPI).");
-	return;
+  if (!config.prefixEnabled || (config.prefixSkippedForMe && selfNotedMessage)) {
+    await handleMessageGPT(client, message, messageString);
+  }
 }
-
-	// DALLE (!dalle <prompt>)
-	if (startsWithIgnoreCase(messageString, config.dallePrefix)) {
-		const prompt = messageString.substring(config.dallePrefix.length + 1);
-		await handleMessageDALLE(message, prompt);
-		return;
-	}
-
-	// Stable Diffusion (!sd <prompt>)
-	if (startsWithIgnoreCase(messageString, config.stableDiffusionPrefix)) {
-		const prompt = messageString.substring(config.stableDiffusionPrefix.length + 1);
-		await executeCommand("sd", "generate", message, prompt);
-		return;
-	}
-
-	// GPT (only <prompt>)
-	if (!config.prefixEnabled || (config.prefixSkippedForMe && selfNotedMessage)) {
-		await handleMessageGPT(message, messageString);
-		return;
-	}
-}
-
-export { handleIncomingMessage };
